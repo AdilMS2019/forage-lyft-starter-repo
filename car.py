e449a1b77a294dbcb0ed8880e8da4a78648e4f30
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-15 16:17:06 +0000

    Move car.py

--- car.py
+++
@@ -1,8 +0,0 @@
-
-class Car:
-    def __init__(self, engine, battery):
-        self.engine = engine
-        self.battery = battery
-
-    def needs_service(self):
-        pass
---
+++ models/car.py
@@ -0,0 +1,11 @@
+from servicable import Servicable
+
+
+class Car(Servicable):
+    def __init__(self, engine, battery):
+        super().__init__()
+        self.engine = None
+        self.battery = None
+
+    def needs_service(self) -> bool:
+        pass
