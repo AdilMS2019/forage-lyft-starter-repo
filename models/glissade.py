3d8ade586c10b56f2f1465addf72d1f2bebb5b18
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-15 17:05:29 +0000

    Alter Car Classes

--- models/glissade.py
+++ models/glissade.py
@@ -5,7 +5,9 @@ from batteries.spindler_battery import SpindlerBattery
 
 class Glissade(Car):
     def __init__(self):
-        super().__init__(WilloughbyEngine, SpindlerBattery)
+        super().__init__()
+        self.engine = WilloughbyEngine
+        self.battery = SpindlerBattery
 
     def needs_service(self):
         return self.battery.needs_service() or self.engine.needs_service()
