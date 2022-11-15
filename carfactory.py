5ac894cb691ade66607a3979db8c21b954936015
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-15 16:18:58 +0000

    Create carfactory.py skeleton

---
+++ carfactory.py
@@ -0,0 +1,21 @@
+from car import Car
+
+
+class CarFactory(Car):
+    def create_calliope() -> Car:
+        pass
+
+    def create_glissade() -> Car:
+        pass
+
+    def create_palindrome() -> Car:
+        pass
+
+    def create_palindrome() -> Car:
+        pass
+
+    def create_rorschach() -> Car:
+        pass
+
+    def create_thovex() -> Car:
+        pass
