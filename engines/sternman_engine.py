2658810139c468ec4949304e6fdf5ec08ca9dfeb
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-15 17:12:36 +0000

    Alter variable name in sternman engine class

--- engines/sternman_engine.py
+++ engines/sternman_engine.py
@@ -2,9 +2,9 @@ from engines.engine import Engine
 
 
 class SternmanEngine(Engine):
-    def __init__(self, warning_light_is_on):
+    def __init__(self, warning_light_on):
         super().__init__()
-        self.warning_light_is_on = warning_light_is_on
+        self.warning_light_on = warning_light_on
 
     def needs_service(self):
-        return self.warning_light_is_on
+        return self.warning_light_on
