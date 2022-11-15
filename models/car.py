a6ddbc018bd23fac461afcd0665e6dc5a6623834
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-15 16:41:10 +0000

    Refactor car class

--- models/car.py
+++ models/car.py
@@ -2,10 +2,10 @@ from servicable import Servicable
 
 
 class Car(Servicable):
-    def __init__(self):
+    def __init__(self, engine, battery):
         super().__init__()
-        self.engine = None
-        self.battery = None
+        self.engine = engine
+        self.battery = battery
 
     def needs_service(self) -> bool:
         pass
