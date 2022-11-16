a2d77a332132e7f82f241b667eebeb6696cc9871
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-16 01:15:49 +0000

    Refactor files

--- batteries/nubbin_battery.py
+++ batteries/nubbin_battery.py
@@ -7,7 +7,7 @@ class NubbinBattery(Battery):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
-    def needs_service(self) -> bool:
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         return service_threshold_date < datetime.today().date()
         
