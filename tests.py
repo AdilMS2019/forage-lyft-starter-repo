56b0e88e83aa73364401e0786fa43d797daa685f
AdilMS2019 <98103227+AdilMS2019@users.noreply.github.com>
2022-11-21 23:21:42 +0000

    Finished tests

--- tests.py
+++ tests.py
@@ -20,11 +20,11 @@ class TestNubbin(unittest.TestCase):
             year=current_date.year - 3), current_date)
         self.assertEqual(battery.needs_service(), False, "Should be false.")
 
-    def should_be_serviced_edge(self):
+    def should_be_serviced_edge_false(self):
         current_date = datetime.today().date()
         battery = NubbinBattery(current_date.replace(
             year=current_date.year - 4), current_date)
-        self.assertEqual(battery.needs_service(), True, "Should be true.")
+        self.assertEqual(battery.needs_service(), False, "Should be false.")
 
 
 class TestSpindler(unittest.TestCase):
@@ -40,44 +40,50 @@ class TestSpindler(unittest.TestCase):
             year=current_date.year - 1), current_date)
         self.assertEqual(battery.needs_service(), False, "Should be false.")
 
-    def should_be_serviced_edge(self):
+    def should_be_serviced_edge_false(self):
         current_date = datetime.today().date()
         battery = SpindlerBattery(current_date.replace(
             year=current_date.year - 2), current_date)
-        self.assertEqual(battery.needs_service(), True, "Should be true.")
+        self.assertEqual(battery.needs_service(), False, "Should be false.")
 
 
 class TestCapulet(unittest.TestCase):
     def should_be_serviced(self):
-        pass
+        engine = CapuletEngine(0, 40000)
+        self.assertEqual(engine.needs_service(), True, "Should be true")
 
     def should_not_be_serviced(self):
-        pass
+        engine = CapuletEngine(0, 20000)
+        self.assertEqual(engine.needs_service(), False, "Should be false")
 
-    def should_be_serviced_edge(self):
-        pass
-
-
-# class TestSternman(unittest.TestCase):
-#     def should_be_serviced(self):
-#         pass
-
-#     def should_not_be_serviced(self):
-#         pass
-
-#     def should_be_serviced_edge(self):
-#         pass
+    def should_be_serviced_edge_false(self):
+        engine = CapuletEngine(0, 30000)
+        self.assertEqual(engine.needs_service(), False, "Should be false")
 
 
-# class TestWilloughby(unittest.TestCase):
-    # def should_be_serviced(self):
-    #     pass
+class TestSternman(unittest.TestCase):
+    def should_be_serviced(self):
+        engine = SternmanEngine(True)
+        self.assertEqual(engine.needs_service(), True, "Should be true")
+        
 
-    # def should_not_be_serviced(self):
-    #     pass
+    def should_not_be_serviced(self):
+        engine = SternmanEngine(False)
+        self.assertEqual(engine.needs_service(), False, "Should be false")
 
-    # def should_be_serviced_edge(self):
-    #     pass
+
+class TestWilloughby(unittest.TestCase):
+    def should_be_serviced(self):
+        engine = WilloughbyEngine(0, 70000)
+        self.assertEqual(engine.needs_service(), True, "Should be true")
+
+    def should_not_be_serviced(self):
+        engine = WilloughbyEngine(0, 20000)
+        self.assertEqual(engine.needs_service(), False, "Should be false")
+
+    def should_be_serviced_edge_false(self):
+        engine = WilloughbyEngine(0, 60000)
+        self.assertEqual(engine.needs_service(), False, "Should be false")
 
 
 if __name__ == '__main__':
